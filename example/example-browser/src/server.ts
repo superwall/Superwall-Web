5fbd92504d4201344f526e2cfbf868558f34d8dc
Ian Rumac <ian.rumac@outlook.com>
2026-06-24 13:41:57 +0200

    Add more examples, add dev host to verifier

--- example/example-browser/src/server.ts
+++ example/example-browser/src/server.ts
@@ -35,6 +35,7 @@ Bun.serve({
         try {
           const result = await verifyEntitlements(body.token, {
             publicApiKey: SUPERWALL_API_KEY,
+            jwksUrl: "https://superwall.dev/.well-known/entitlements/jwks.json",
           });
           return json({
             verified: true,
